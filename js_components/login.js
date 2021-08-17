7df529bb7ccccd1d73df685c401d9cd74b69a633
Brandon <brandon121j@DESKTOP-DPFP3JR.localdomain>
2021-08-17 19:30:49 -0400

    Completion of Register.js file and register.html file

---
+++ js_components/login.js
@@ -0,0 +1,18 @@
+let userInfo = localStorage.getItem('userData')
+
+console.log(userInfo)
+
+const username = $('#userName input');
+
+const password = $('#password input');
+
+const button = $('button');
+
+let loginValidator = {
+    user: false,
+    pass: false
+}
+
+const submit = () => {
+    
+}
---
+++ js_components/register.js
@@ -0,0 +1,78 @@
+const button = $('button')
+
+const username = $('#registerUsername input')
+
+const password = $('#registerPassword input');
+
+const confirm = $('#confirmPassword input');
+
+const usernameMessage = $('#registerUsername p');
+
+const passwordMessage = $('#registerPassword p');
+
+const confirmMessage = $('#confirmPassword p');
+
+const usernameError = '* username must be longer than 2 characters';
+
+const passwordError = '* password must be longer than 2 characters';
+
+const confirmError = '* Passwords must match';
+
+let registerValidator = {
+    user: false,
+    pass: false,
+    confirm: false 
+}
+
+const submit = () => {
+    let userData = {
+        username: username.val(),
+        password: password.val()
+    }
+
+    if (registerValidator.user === true &&
+        registerValidator.pass === true &&
+        registerValidator.confirm === true) {
+            localStorage.setItem('userData', JSON.stringify(userData))
+            window.location.href = "/html_components/index.html"
+        }
+}
+
+const validatorStatus = () => {
+    let passValue = password.val();
+    let confirmValue = confirm.val();
+    let usernameValue = username.val();
+    confirmValue === passValue && confirmValue.length > 3 ? registerValidator.confirm = true : registerValidator.confirm = false; 
+    usernameValue.length > 3 ? registerValidator.user = true : registerValidator.user = false;
+    passValue.length > 3 ? registerValidator.pass = true : registerValidator.pass = false;
+}
+
+const usernameValidator = () => {
+    let usernameValue = username.val();
+    usernameValue.length < 3 ? username.addClass('error') && usernameMessage.html(usernameError) : username.removeClass('error') && username.addClass('valid') && usernameMessage.html('');
+}
+
+const passwordValidator = () => {
+    let passValue = password.val();
+    passValue.length < 3 ? password.addClass('error') && passwordMessage.html(passwordError) : password.removeClass('error') && password.addClass('valid') && passwordMessage.html('');
+}
+
+const confirmValidator = () => {
+    let confirmValue = confirm.val();
+    let passValue = password.val();
+    confirmValue !== passValue ? confirm.addClass('error') && confirmMessage.html(confirmError) :confirm.removeClass('error') &&confirm.addClass('valid') &&confirmMessage.html('');
+}
+
+$('input').on('change', validatorStatus);
+
+username.on('blur', usernameValidator);
+
+password.on('blur', passwordValidator);
+
+confirm.on('blur', confirmValidator);
+
+button.on('click', submit);
+
+
+
+
